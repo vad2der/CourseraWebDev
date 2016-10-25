bc11cb23ed13b4f99c054e2c107f531a7509aff5
vad2der <vad.deryabin@gmail.com>
2016-10-25 13:41:10 -0600

    have to deploy to try directive, otherwise getiing crossorigin error

---
+++ course5_assignment3/app.js
@@ -0,0 +1,72 @@
+(function () {
+'use strict';
+
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com/")
+.directive('foundItems', foundItemsDirective);
+
+function foundItemsDirective() {
+  var ddo = {
+    templateUrl: 'menuItemList.html',
+    scope: {
+      items: '<',
+      onRemove: '&'
+    },
+    controller: NarrowItDownController,
+    controllerAs: 'menu',
+    bindToController: true
+  };
+
+  return ddo;
+}
+
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
+  var menu = this;
+  menu.searchParam = "";
+
+  menu.searchItems = function () {    
+    var promise = MenuSearchService.getMatchedMenuItems(menu.searchParam);    
+    promise.then(function (result) {
+      menu.found  = result;
+      menu.foundBool = true;
+
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+  };
+
+  menu.deleteThisItem = function(itemIndex){
+    menu.found.splice(itemIndex, 1);
+  };
+
+}
+
+
+MenuSearchService.$inject = ['$http', 'ApiBasePath']
+function MenuSearchService($http, ApiBasePath) {
+  var service = this;
+
+  service.getMatchedMenuItems = function (searchParam) {
+    return $http({
+      method: "GET",
+      url: (ApiBasePath + "/menu_items.json")
+    }).then(function(result){
+      var foundItems= [];
+      if (searchParam != undefined){
+        for (var r_ind in result.data.menu_items){          
+          if (result.data.menu_items[r_ind].description.toString().toLowerCase().includes(searchParam.toString().toLowerCase()) 
+            || result.data.menu_items[r_ind].name.toString().toLowerCase().includes(searchParam.toString().toLowerCase())){
+            foundItems.push(result.data.menu_items[r_ind]);
+          };
+        };  
+      };
+      
+      return foundItems;
+    });    
+  };
+}
+})();
