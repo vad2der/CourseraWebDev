a449b6de4c31e9fa1d18c277081779221ad267a6
vad2der <vad.deryabin@gmail.com>
2016-11-27 15:41:01 -0700

    get the bug

--- course5_assignment5/src/public/user/my-info.controller.js
+++ course5_assignment5/src/public/user/my-info.controller.js
@@ -7,9 +7,9 @@
 	MyInfoController.$inject = ['MenuService', 'AccountService', 'ApiPath'];
 	function MyInfoController(MenuService, AccountService, ApiPath){
 		var accountCtrl = this;
-		accountCtrl.account = AccountService.getAccount();				
-		accountCtrl.account.path = ApiPath;
+		accountCtrl.account = AccountService.getAccount();						
 		if (accountCtrl.account){
+			accountCtrl.account.path = ApiPath;
 			var promise = MenuService.getMenuItemByShortName(accountCtrl.account.favoriteDish);
 			promise.then(function(result){accountCtrl.account.favoriteDish = promise.$$state.value})
 		};
