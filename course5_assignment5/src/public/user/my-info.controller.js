47f2cdd79efc44ea61bf053a28df419105753c5d
vad2der <vad.deryabin@gmail.com>
2016-11-27 15:38:47 -0700

    get the bug

--- course5_assignment5/src/public/user/my-info.controller.js
+++ course5_assignment5/src/public/user/my-info.controller.js
@@ -8,8 +8,10 @@
 	function MyInfoController(MenuService, AccountService, ApiPath){
 		var accountCtrl = this;
 		accountCtrl.account = AccountService.getAccount();		
-		var promise = MenuService.getMenuItemByShortName(accountCtrl.account.favoriteDish);
-		promise.then(function(result){accountCtrl.account.favoriteDish = promise.$$state.value})		
+		var promise = MenuService.getMenuItemByShortName(accountCtrl.account.favoriteDish);				
 		accountCtrl.account.path = ApiPath;
+		if (accountCtrl.account){
+			promise.then(function(result){accountCtrl.account.favoriteDish = promise.$$state.value})
+		};
 	};
 })();
